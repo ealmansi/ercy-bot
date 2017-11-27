eea7e593ce1ad88c5c37d5bc6f9dd5183be7b7a0
Emilio Almansi <ealmansi@gmail.com>
2017-11-27 03:45:24 -0300

    Extract config.

--- truffle/truffle.js
+++ truffle/truffle.js
@@ -1,39 +1,19 @@
-// require('babel-register');
-
-// module.exports = {
-//   networks: {
-//     development: {
-//       host: 'localhost',
-//       port: 8545,
-//       gas: 4700036,
-//       network_id: '*'
-//     },
-//     ropsten: {
-//       host: 'ropsten.smartcontracts.xyz',
-//       port: 8565,
-//       gas: 4700036,
-//       network_id: "3",
-//     }
-//   }
-// };
-
-var HDWalletProvider = require("truffle-hdwallet-provider");
-
-var infura_apikey = "3zxxgIUG8QdF9cKWbLQP";
-var mnemonic = "learn digital grit wide toddler mobile select run drill light bicycle dove";
+require('babel-register');
+const config = require('./config');
 
 module.exports = {
   networks: {
     development: {
-      host: "localhost",
+      host: 'localhost',
       port: 8545,
       gas: 4700036,
-      network_id: "*" // Match any network id
+      network_id: '*'
     },
     ropsten: {
-      provider: new HDWalletProvider(mnemonic, "https://ropsten.infura.io/"+infura_apikey),
+      host: config.ropsten.host,
+      port: config.ropsten.port,
       gas: 4700036,
-      network_id: 3
+      network_id: "3",
     }
   }
 };
