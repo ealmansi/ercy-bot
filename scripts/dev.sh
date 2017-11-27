0c7a61504ee5dbb2a784a659c8c58d319c0e5f1b
Emilio Almansi <ealmansi@gmail.com>
2017-11-27 03:31:13 -0300

    Refactored polling logic. Added mock Slack api. Multiple contract support.

---
+++ scripts/dev.sh
@@ -0,0 +1,1 @@
+./node_modules/.bin/concurrently --kill-others --names "REDIS,SLACK,BOT" 'node start-redis.js' 'node mock-slack.js' 'sleep 1 && node index.js'
--- src/DatabaseFactory.js
+++ src/DatabaseFactory.js
@@ -1,52 +1,115 @@
 const logger = require('./logger');
 const Promise = require('bluebird');
-const RedisServer = require('redis-server');
 const redis = Promise.promisifyAll(require('redis'));
+const leftPad = require('left-pad');
 
-class DatabaseClient {
+const KEY_SEPARATOR = ':';
+const ENCODING_PREFIX_LENGTH = 2;
+
+class DatabaseFactory {
   
-  constructor(redisClient) {
-    this.redisClient = redisClient;
-  }
-
-  async pushEvent(eventId) {
-    await this.redisClient.rpushAsync('events', eventId);
-  }
-
-  async peekEvent() {
-    let [ eventId ] = await this.redisClient.lrangeAsync('events', 0, 0);
-    return eventId;
-  }
-
-  async popEvent() {
-    await this.redisClient.lpop('events');
+  static createClient(namespace, cacheTtl) {
+    return new Promise((resolve, reject) => {
+      const redisClient = redis.createClient();
+      redisClient.on('ready', () => {
+        resolve(new DatabaseClient(namespace, cacheTtl, redisClient));
+      });
+      redisClient.on('error', reject);
+    });
   }
 };
 
-class DatabaseFactory {
-
-  static setup(env) {
-    if (env === 'development') {
-      logger.info('Starting Redis development server.');
-      return new Promise((resolve, reject) => {
-        const server = new RedisServer();
-        server.open((err) => {
-          if (err !== null) {
-            reject(err);
-          }
-          resolve();
-        });
-      });
-    }
-    return Promise.resolve();
+class DatabaseClient {
+  
+  constructor(namespace, cacheTtl, redisClient) {
+    this.namespace = namespace;
+    this.redisClient = redisClient;
+    this.blockNumberKey = this.addNamespace('block');
+    this.transfersKey = this.addNamespace('transfers');
+    this.cacheTtl = cacheTtl;
   }
 
-  static createClient() {
-    return new Promise((resolve, reject) => {
-      const redisClient = redis.createClient();
-      redisClient.on('ready', () => resolve(new DatabaseClient(redisClient)));
-      redisClient.on('error', reject);
-    });
+  async getPendingBlockNumber() {
+    const blockNumber = await this.redisClient.getAsync(this.blockNumberKey);
+    return blockNumber !== null ? Number(blockNumber) : null;
+  }
+
+  async setPendingBlockNumber(blockNumber) {
+    await this.redisClient.setexAsync(this.blockNumberKey, this.cacheTtl, blockNumber);
+  }
+
+  async addTransfer(transfer) {
+    const transferKey = this.makeTransferKey(transfer);
+    const exists = await this.redisClient.existsAsync(transferKey) === 1;
+    if (!exists) {
+      const fields = this.flatten(Object.entries(transfer));
+      const transferId = this.makeTransferId(transfer);
+      await this.redisClient.multi()
+      .hmset(transferKey, ...fields)
+      .expire(transferKey, this.cacheTtl)
+      .zadd(this.transfersKey, 0, this.encodeTransferId(transferId))
+      .expire(this.transfersKey, this.cacheTtl)
+      .execAsync();
+    }
+  }
+
+  async nextTransfer() {
+    const range = await this.redisClient.zrangebyscoreAsync(this.transfersKey, ...[
+      0, 0, 'LIMIT', 0, 1
+    ]);
+    if (range.length === 0) {
+      return null;
+    }
+    const transferId = this.decodeTransferId(range[0]);
+    const transferKey = this.makeTransferKeyFromId(transferId);
+    return await this.redisClient.hgetallAsync(transferKey);
+  }
+
+  async removeTransfer(transfer) {
+    const transferId = this.makeTransferId(transfer);
+    await this.redisClient.zremAsync(this.transfersKey, this.encodeTransferId(transferId));
+  }
+
+  makeTransferId(transfer) {
+    return [transfer.blockNumber, transfer.logIndex].join(KEY_SEPARATOR);
+  }
+
+  makeTransferKeyFromId(transferId) {
+    return this.addNamespace(['tx', transferId].join(KEY_SEPARATOR));
+  }
+  
+  makeTransferKey(transfer) {
+    return this.makeTransferKeyFromId(this.makeTransferId(transfer));
+  }
+
+  encodeTransferId(transferId) {
+    const [blockNumber, logIndex] = transferId.split(KEY_SEPARATOR);
+    return [
+      this.encodeLength(blockNumber),
+      blockNumber,
+      this.encodeLength(logIndex),
+      logIndex
+    ].join(KEY_SEPARATOR);
+  }
+
+  encodeLength(number) {
+    return leftPad(number.toString().length, ENCODING_PREFIX_LENGTH, '0');
+  }
+
+  decodeTransferId(encodedTransferId) {
+    const [_, blockNumber, __, logIndex] = encodedTransferId.split(KEY_SEPARATOR);
+    return [
+      blockNumber,
+      logIndex
+    ].join(KEY_SEPARATOR);
+  }
+
+  addNamespace(baseKey) {
+    return [this.namespace, baseKey].join(KEY_SEPARATOR);
+  }
+
+  flatten(array) {
+    return array.reduce((result, element) => result.concat(element), []);
   }
 };
 
